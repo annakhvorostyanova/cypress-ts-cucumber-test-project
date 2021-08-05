5a764c293ab9f6d039c8a4a8167d798f21027acb
DarthNekko <88428743+DarthNekko@users.noreply.github.com>
2021-08-05 15:43:47 +0400

    Update steps.ts

--- cypress/integration/test-site/features/checkElementsOnThePage/steps.ts
+++ cypress/integration/test-site/features/checkElementsOnThePage/steps.ts
@@ -1,17 +1,20 @@
 import { When, Then } from 'cypress-cucumber-preprocessor/steps';
-import FifthCoursePage from '../../../../pages/test-site/FifthCoursePage';
 import MainPage from "../../../../pages/test-site/MainPage";
+import SixthYearPage from '../../../../pages/test-site/SixthYearPage';
 
 When(/^I open the main page$/, () => {
-  MainPage.visit();
+  MainPage.openTermSix();
 });
 Then(/^I see all the elements on the main page$/, () => {
   MainPage.checkElementsOnThePage();
 });
 
-When(/^I open the fifth course page$/, () => {
-  MainPage.openTermNineTen();
+When(/^I open the sixth year section$/, () => {
+  cy.visit('/');
+ 
 });
-Then(/^I see all the elements on the fifth course page$/, () => {
-  FifthCoursePage.checkElementsOnThePage();
+Then(/^I see all the elements on the sixth year section page$/, () => {
+  SixthYearPage.checkElementsVisibility();
+  
+  console.log('checking')
 });
