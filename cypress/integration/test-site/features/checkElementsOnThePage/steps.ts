9bd6fdd76cfc58efc6e8a064c9e143addea97cbb
DarthNekko <88428743+DarthNekko@users.noreply.github.com>
2021-08-05 13:37:49 +0400

    Update steps.ts

--- cypress/integration/test-site/features/checkElementsOnThePage/steps.ts
+++ cypress/integration/test-site/features/checkElementsOnThePage/steps.ts
@@ -1,9 +1,20 @@
 import { When, Then } from 'cypress-cucumber-preprocessor/steps';
 import MainPage from "../../../../pages/test-site/MainPage";
+import SixthYearPage from '../../../../pages/test-site/SixthYearPage';
 
 When(/^I open the main page$/, () => {
   MainPage.visit();
 });
 Then(/^I see all the elements on the main page$/, () => {
   MainPage.checkElementsOnThePage();
+});
+
+When(/^I open the sixth year section$/, () => {
+  cy.visit('/');
+ 
+});
+Then(/^I see all the elements on the sixth year section page$/, () => {
+  SixthYearPage.checkElementsVisibility();
+  
+  console.log('checking')
 });
